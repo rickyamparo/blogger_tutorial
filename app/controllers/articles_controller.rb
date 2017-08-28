c2ecbb5f78275eadac1f1e0025181734173b837b
Ricky Amparo <rickyamparo@gmail.com>
2017-08-27 21:38:00 -0600

    add tagging functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -51,7 +51,7 @@ class ArticlesController < ApplicationController
   end
 
   def article_params
-    params.require(:article).permit(:title, :body)
+    params.require(:article).permit(:title, :body, :tag_list)
   end
 
 end
